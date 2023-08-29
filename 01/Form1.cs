4fd5ab16e2ee189c058ea046f6753ac7e59829e9
hutarj <100676664+hutarj@users.noreply.github.com>
2023-08-29 19:22:13 +0200

    Příklad 1

--- 01/Form1.cs
+++ 01/Form1.cs
@@ -16,5 +16,46 @@ namespace _01
         {
             InitializeComponent();
         }
+        /*
+        Napište metodu, která ověří, že zadané číslo je prvočíslo. Algoritmus v metodě
+        maximálně optimalizujte.
+        Na vstupu je číslo N a N-prvková posloupnost náhodných celých čísel z intervalu 1..120
+        včetně. Obsahuje posloupnost prvočíslo a jaké je první z nich? Pro zkrácení cyklu
+        procházejícího prvky posloupnosti použijte typ bool.
+        */
+
+        private bool JePrvocislo(int cislo)
+        {
+            bool jePrvocislo = true;
+            if (cislo == 1 || (cislo % 2 == 0 && cislo != 2)) jePrvocislo = false;
+            for (int i = 3; i < Math.Sqrt(cislo) && jePrvocislo; i+=2)
+            {
+                if (cislo % i == 0) jePrvocislo = false;
+            }
+
+            return jePrvocislo;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int n = int.Parse(textBox1.Text);
+
+            listBox1.Items.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                int cislo = rnd.Next(1, 121);
+                listBox1.Items.Add(cislo);
+            }
+
+            bool obsahujePrvocislo = false;
+            for (int i = 0; i < listBox1.Items.Count && !obsahujePrvocislo; i++)
+            {
+                int cislo = int.Parse(listBox1.Items[i].ToString());
+                if (this.JePrvocislo(cislo)) {
+                    obsahujePrvocislo = true;
+                    MessageBox.Show($"První prvočíslo je {cislo}");
+                }
+            }
+        }
     }
 }
