6e9e9da131bf845561aedb4bfbe2e4c7bdc5eed0
hutarj <100676664+hutarj@users.noreply.github.com>
2023-08-29 19:35:10 +0200

    Příklad 3

---
+++ 03/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+        Navrhněte projekt, který bude obsahovat metodu JePrvocislo s parametrem typu int.
+        Metoda zjistí, zda zadané číslo je prvočíslo. Algoritmus v metodě, co nejvíce
+        optimalizujte. Vypište největší prvočíslo menší než 1000.
+        */
+
+        private bool JePrvocislo(int cislo)
+        {
+            bool jePrvocislo = true;
+            if (cislo == 1 || (cislo % 2 == 0 && cislo != 2)) jePrvocislo = false;
+            for (int i = 3; i < Math.Sqrt(cislo) && jePrvocislo; i += 2)
+            {
+                if (cislo % i == 0) jePrvocislo = false;
+            }
+
+            return jePrvocislo;
+            {
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nejvetsiPrvocislo = -1;
+            for (int i = 1; i < 1001; i++)
+            {
+                if (this.JePrvocislo(i)) nejvetsiPrvocislo = i;
+            }
+            if (nejvetsiPrvocislo != -1) MessageBox.Show($"Největší prvočíslo je {nejvetsiPrvocislo}");
+            else MessageBox.Show("Neobsahuje prvočíslo");
+        }
+    }
+}
